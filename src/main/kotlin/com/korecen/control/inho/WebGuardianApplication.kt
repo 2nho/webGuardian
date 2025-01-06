fc03ddb106c4c753b3af083c90f6531891fc01ba
kim2nho <kim2nho@gmail.com>
2025-01-06 15:07:56 +0900

    Initial commit

---
+++ src/main/kotlin/com/korecen/control/inho/WebGuardianApplication.kt
@@ -0,0 +1,11 @@
+package com.korecen.control.inho
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebGuardianApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebGuardianApplication>(*args)
+}
---
+++ src/test/kotlin/com/korecen/control/inho/WebGuardianApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.korecen.control.inho
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebGuardianApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
